0f55589358c69e6597183f0d23b2f7194b90a9e4
marongayo <sir.ongayo@gmail.com>
2021-11-25 19:04:04 +0300

    resolving image alteration

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from posts.models import Posts
 
 def index(request):
-    queryset = Posts.objects.filter(featured = True)
+    featured = Posts.objects.filter(featured = True)
+    latest = Posts.objects.order_by('-timestamp')[0:3]
+
     return render(request, 'index.html', {
-        'object_list':queryset
+        'object_list':featured,
+        'latest':latest
     })
 
 def blog(request):
